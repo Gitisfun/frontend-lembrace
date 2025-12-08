a7d67ee5a3738e36f6ab4cdad7c2157dc42752f7
Christian <christian.vandeputtelaar@bosa.fgov.be>
2025-12-08 01:04:18 +0100

    Update favico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', href: '/icon.png' },
         { rel: 'canonical', href: 'https://lembrace.be' },
       ],
       meta: [
